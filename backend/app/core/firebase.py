02d79152974d714d8a0ce3a97ed8436324b58f36
choi-siwon1106 <chltlnds1106@gamil.com>
2026-09-06 17:41:04 +0900

    Support Firebase credentials from environment

--- backend/app/core/firebase.py
+++ backend/app/core/firebase.py
@@ -6,8 +6,22 @@ from dotenv import load_dotenv
 load_dotenv()
 
 if not firebase_admin._apps:
-    cred_path = os.getenv("FIREBASE_CREDENTIALS_PATH", "serviceAccountKey.json")
-    cred = credentials.Certificate(cred_path)
+    project_id = os.getenv("FIREBASE_PROJECT_ID")
+    client_email = os.getenv("FIREBASE_CLIENT_EMAIL")
+    private_key = os.getenv("FIREBASE_PRIVATE_KEY")
+
+    if project_id and client_email and private_key:
+        cred = credentials.Certificate({
+            "type": "service_account",
+            "project_id": project_id,
+            "private_key": private_key.replace("\\n", "\n"),
+            "client_email": client_email,
+            "token_uri": "https://oauth2.googleapis.com/token",
+        })
+    else:
+        cred_path = os.getenv("FIREBASE_CREDENTIALS_PATH", "serviceAccountKey.json")
+        cred = credentials.Certificate(cred_path)
+
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
